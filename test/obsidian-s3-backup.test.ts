93b72656b48172174998c465332601886e360e13
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2024-02-05 13:51:52 +0000

    Initial commit

---
+++ test/obsidian-s3-backup.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ObsidianS3Backup from '../lib/obsidian-s3-backup-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/obsidian-s3-backup-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ObsidianS3Backup.ObsidianS3BackupStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
